feeabac221744c551307e625f9b0427e17664234
volkov <mr.rotberry@gmail.com>
2019-04-18 14:46:27 +0300

    Updates

---
+++ precache-manifest.09dd443b06e0b73806bdf3f7adae1d74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b92084b953f68e6ad29",
+    "url": "/eOCDS-schema-validator/static/css/main.70b1339b.chunk.css"
+  },
+  {
+    "revision": "8b92084b953f68e6ad29",
+    "url": "/eOCDS-schema-validator/static/js/main.803f49aa.chunk.js"
+  },
+  {
+    "revision": "4a8f6b81f2c2239eec9c",
+    "url": "/eOCDS-schema-validator/static/js/runtime~main.3afb529a.js"
+  },
+  {
+    "revision": "93057f468775505a2018",
+    "url": "/eOCDS-schema-validator/static/css/2.a50c00eb.chunk.css"
+  },
+  {
+    "revision": "93057f468775505a2018",
+    "url": "/eOCDS-schema-validator/static/js/2.2727c2c7.chunk.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/eOCDS-schema-validator/static/media/jsoneditor-icons.d961fdfa.svg"
+  },
+  {
+    "revision": "423fc7971c579de173e273844aec8a69",
+    "url": "/eOCDS-schema-validator/index.html"
+  }
+];
